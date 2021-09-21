5d6c9284a57d8df92351a81bc5653a9d70933c6a
perfect6566 <perfect6566@126.com>
2021-09-21 11:00:26 +0800

    Update classvar.py

--- classvar.py
+++ classvar.py
@@ -1,24 +1,37 @@
+from memory_profiler import profile
+import psutil,os,gc
 paraalist=[]
-class BaseA():
-    # print("类变量")
-    # a=[]
-    # for i in range(10000000):
-    #     a.append(i)
-    #     paraalist.append(i)
+class BaseAClassVars():
+    print("类变量，会在此模块加载中自动执行1次")
+    a=[]
+    for i in range(10000000):
+        a.append(i)
+        paraalist.append(i)
+
+class BaseAClassInstancesVars():
+
     def __init__(self):
-        print("类实例变量")
+        print("类实例变量，每次实例化的时候都会执行")
         self.a=[]
-
         for i in range(10000000):
-
             self.a.append(i)
             paraalist.append(i)
 
-class A(BaseA):
-
+class A(BaseAClassVars):
+    @profile
     def __init__(self):
-        self.aa=BaseA()
+        self.aa=BaseAClassVars()
 
-for i in range(10000000):
+class AA(BaseAClassInstancesVars):
+    @profile
+    def __init__(self):
+        self.aa=BaseAClassInstancesVars()
 
-    A()
+def main():
+    for i in range(10000):
+        # A()
+        AA()
+
+if __name__=="__main__":
+    main()
+
